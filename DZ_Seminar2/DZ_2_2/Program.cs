d1480a0768666eeb00274b497fe17228a99399af
IvanLozovik <gliderpilot19@gmail.com>
2023-08-09 21:28:48 +0300

    DZ po vtoromu Seminaru

---
+++ DZ_Seminar2/DZ_2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+    Console.Write("Введите число: ");
+    string numStr1 = Console.ReadLine();
+    int number1 = int.Parse(numStr1);
+    //int number1 = new Random().Next( 0, 1000000000);   
+    // Console.WriteLine("Исходное число: " + number1);
+    if (number1/100==0)
+    {
+        Console.WriteLine("Третьей цифры нет!");
+        return;
+    }
+    while (number1/1000 > 0)  
+    {
+        number1=number1/10;
+    }  
+Console.WriteLine("Третья цифра = " + number1 % 10);
---
+++ DZ_Seminar2/DZ_2_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным
+System.Console.Write("Введите номер дня недели: ");
+string dayNumber = Console.ReadLine();
+int dayNum = int.Parse(dayNumber);
+switch (dayNum)
+{
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        System.Console.WriteLine("Нет. Не выходной.");
+        break;
+    case 6:
+    case 7:
+        System.Console.WriteLine("Да. Выходной!");
+        break;
+    default:
+        System.Console.WriteLine("Неверный номер");
+        break;
+}
