57c4aaacbada8f9fb82f94b9313d177634fdb4ff
IvanLozovik <gliderpilot19@gmail.com>
2023-08-31 23:02:10 +0300

    DZ po 4 Seminaru

---
+++ DZ_Seminar4_Met/DZ4_1_Met/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int GetNumStepen (int num, int stepen)
+{
+    int result=1;
+    
+    while (stepen > 0) 
+    { 
+    result *= num; 
+    stepen--;
+    //System.Console.WriteLine(result); 
+    }
+return result;
+}
+// int res = GetNumStepen (3,5);   
+// System.Console.WriteLine(res);
+System.Console.WriteLine($"{GetNumStepen (2,4)}");
---
+++ DZ_Seminar4_Met/DZ4_3_Met/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void PrintMassive (int[] array)
+{
+    Console.Write("[");
+    foreach (var item in array) 
+    {
+    //Console.WriteLine (array [i]);
+    System.Console.Write($"{item}");
+    }
+    Console.WriteLine ("]");
+}
+
+int [] GetMassive (int size)
+{
+    int [] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        array [i] = new Random().Next(0, 10);
+     
+    }
+    return array;
+}
+
+PrintMassive (GetMassive(8));
+
