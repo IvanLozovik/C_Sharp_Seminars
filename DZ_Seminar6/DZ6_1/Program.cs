6ab8169ff5dad00a71a492a06853cf1b4c17413c
IvanLozovik <gliderpilot19@gmail.com>
2023-09-04 00:18:37 +0300

    DZ po 6 Seminaru - part 1

---
+++ DZ_Seminar6/DZ6_1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int GetNumbersMoreZero (int[] array)
+{
+int count = 0;
+for (int i=0; i < array.Length; i++)
+    {
+        if (array [i] > 0) 
+        {
+        count+=1;
+        }
+    }
+return count;
+}
+
+
+int [] GenerateMassive (int Len)
+{
+    int [] array = new int [Len];
+    for (int i = 0; i < Len; i++)
+    {
+        array [i] = new Random().Next(-100, 100);
+     
+    }
+    return array;
+}
+
+void PrintMassive (int[] array)
+{
+    Console.Write("[");
+    foreach (var item in array) 
+    {
+     System.Console.Write($"{item} ");
+    }
+    Console.WriteLine ("]");
+}
+
+int [] Massive = GenerateMassive (10);
+PrintMassive (Massive);
+System.Console.WriteLine ($"{GetNumbersMoreZero(Massive)}");
+
