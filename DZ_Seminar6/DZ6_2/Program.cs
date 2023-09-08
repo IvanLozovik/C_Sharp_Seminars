6d8b76f68b623d14b59078a80391a726908ac8a6
IvanLozovik <gliderpilot19@gmail.com>
2023-09-08 14:29:16 +0300

    DZ po 6 Seminaru - part 2

--- DZ_Seminar6/DZ6_2/Program.cs
+++ DZ_Seminar6/DZ6_2/Program.cs
@@ -3,3 +3,14 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double [] GetTwoLinesCrossPoint (double b1, double k1, double b2, double k2)
+{
+
+    double CrossPointX = (b1-b2)/(k2-k1);
+    double CrossPointY = k1*CrossPointX +b1;
+    double [] CrossPoint = {CrossPointX, CrossPointY};
+    return CrossPoint ; 
+}
+double[] coord = GetTwoLinesCrossPoint(2,5,4,9);
+System.Console.WriteLine    ($"{coord[0]}, {coord [1]}");
