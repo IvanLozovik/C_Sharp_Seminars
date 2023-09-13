340820e052871fc58e27541eb223b946a0a2811a
IvanLozovik <gliderpilot19@gmail.com>
2023-09-14 01:23:13 +0300

    DZ po 7 & 8 Seminaru - part 4

--- DZ_Seminar8/DZ_8_3/Program.cs
+++ DZ_Seminar8/DZ_8_3/Program.cs
@@ -8,9 +8,23 @@
 
 int [,] GetMultiMatrix (int [,] matrixA, int [,] matrixB)
 {
-    int [,] matrixC = new int [,] { matrixA.GetLength (0) };
+    int [,] matrixC = new int [ matrixA.GetLength (0), matrixB.GetLength (1) ];
+    for (int i=0; i< matrixA.GetLength(0); i ++)
+    { 
+        // int summ=0;
+        for (int j=0; j< matrixB.GetLength(1); j ++)
+        {
+              int summ=0;
+            for (int k=0; k< matrixA.GetLength(1); k ++)
+            { 
+                    matrixC [i,j]+= matrixA[i,k]*matrixB[k,j];    
+            }
+            matrixC [i,j] += summ;
+        }
+        
+    }
+    return matrixC;
 }
-
 int [,] CreateRandomMatrix(int m=2, int n=2, int minLimitRandom=1, int maxLimitRandom=10)
     {
         int[,] matrix = new int[m, n];
@@ -36,3 +50,11 @@ void PrintArray(int[,] matrix)
         System.Console.WriteLine();
     }
 }
+
+int [,] matricaA = CreateRandomMatrix(3,2,1,4);
+int [,] matricaB = CreateRandomMatrix(2,3,1,5);
+PrintArray (matricaA);
+System.Console.WriteLine();
+PrintArray (matricaB);
+System.Console.WriteLine();
+PrintArray (GetMultiMatrix(matricaA, matricaB));
--- DZ_Seminar8/DZ_8_5/Program.cs
+++ DZ_Seminar8/DZ_8_5/Program.cs
@@ -1,55 +1,161 @@
 ﻿
-// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
-// Например, на выходе получается вот такой массив:
-// 01 02 03 04
-// 12 13 14 05
-// 11 16 15 06
-// 10 09 08 07
+// // // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// // // Например, на выходе получается вот такой массив:
+// // // 01 02 03 04
+// // // 12 13 14 05
+// // // 11 16 15 06
+// // // 10 09 08 07
 
-int[,] CreateSpiralMatrix(int n, int m, int beginNumber=1)
-    {
-        int[,] matrix = new int[n, m];
-        int znachenie = beginNumber;
-        for (int i = 0; i < n; i++)
-        {   
-            for (int j = 0; j < m; j++)
+// // int[,] CreateSpiralMatrix(int n, int m, int beginNumber=1)
+// //     {
+// //         int[,] matrix = new int[n, m];
+// //         int znachenie = beginNumber;
+// //         for (int i = 0; i < n; i++)
+// //         {   
+// //             for (int j = 0; j < m; j++)
+// //             {
+// //             matrix[i, j] = znachenie;
+// //             znachenie+=1;
+// //             }
+// //             for (int i = 0; i < n; i++)
+// //         }
+// //         return matrix;
+
+// //     double[,] CreateRandomMatrix(int m, int n, int minLimitRandom, int maxLimitRandom)
+// //     {
+// //         // Введите свое решение ниже
+// //         double[,] matrix = new double[m, n];
+// //         Random r = new Random();
+// //         for (int i = 0; i < m; i++)
+// //         {   
+// //             for (int j = 0; j < n; j++)
+// //             {
+// //             matrix[i, j] = r.NextDouble() * (maxLimitRandom - minLimitRandom)+minLimitRandom;
+
+// //             }
+// //         }
+// //         return matrix;
+// //     }
+
+// // void PrintArray(double[,] matrix)
+// // {
+// //     // Введите свое решение ниже
+// //     for (int i = 0; i < matrix.GetLength(0); i++)
+// //     {
+// //         for (int j = 0; j < matrix.GetLength(1); j++)
+// //         {
+// //             System.Console.Write($"{matrix[i, j]:f2} ");
+// //         }
+// //         System.Console.WriteLine();
+// //     }
+// // }
+
+
+// // double[,] result = CreateRandomMatrix(6, 7, -100, 300);
+// // PrintArray(result);
+
+
+// int n = 8; // Размерность массива
+//         int[,] array = new int[n, n]; // Создание двумерного массива
+        
+//         int value = 1; // Начальное значение для заполнения массива
+        
+//         int row = 0; // Текущая строка
+//         int col = 0; // Текущий столбец
+//         int rowDir = 0; // Направление движения по строкам (0 - вправо, 1 - вниз, 2 - влево, 3 - вверх)
+//         int colDir = 1; // Направление движения по столбцам (0 - вправо, 1 - вниз, 2 - влево, 3 - вверх)
+        
+//         // Заполнение массива
+//         for (int i = 0; i < n * n; i++)
+//         {
+//             array[row, col] = value;
+//             value++;
+            
+//             // Проверка следующей позиции
+//             int nextRow = row + rowDir;
+//             int nextCol = col + colDir;
+            
+//             if (nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= n || array[nextRow, nextCol] != 0)
+//             {
+//                 // Изменение направления движения
+//                 int temp = rowDir;
+//                 rowDir = colDir;
+//                 colDir = -temp;
+//             }
+            
+//             // Обновление текущей позиции
+//             row += rowDir;
+//             col += colDir;
+//         }
+        
+//         // Вывод массива на консоль
+//         for (int i = 0; i < n; i++)
+//         {
+//             for (int j = 0; j < n; j++)
+//             {
+//                 Console.Write(array[i, j] + "\t");
+//             }
+//             Console.WriteLine();
+//         }
+
+ int n = 5; // Размерность массива
+        int[,] array = new int[n, n]; // Создание двумерного массива
+        
+        int value = 1; // Начальное значение для заполнения массива
+        
+        int rowStart = 0; // Индекс начальной строки
+        int rowEnd = n - 1; // Индекс конечной строки
+        int colStart = 0; // Индекс начального столбца
+        int colEnd = n - 1; // Индекс конечного столбца
+        
+        // Заполнение массива
+        while (rowStart <= rowEnd && colStart <= colEnd)
+        {
+            // Заполнение верхней строки
+            for (int col = colStart; col <= colEnd; col++)
             {
-            matrix[i, j] = znachenie;
-            znachenie+=1;
+                array[rowStart, col] = value;
+                value++;
             }
-            for (int i = 0; i < n; i++)
+            rowStart++;
+            
+            // Заполнение правого столбца
+            for (int row = rowStart; row <= rowEnd; row++)
+            {
+                array[row, colEnd] = value;
+                value++;
+            }
+            colEnd--;
+            
+            // Заполнение нижней строки
+            if (rowStart <= rowEnd)
+            {
+                for (int col = colEnd; col >= colStart; col--)
+                {
+                    array[rowEnd, col] = value;
+                    value++;
+                }
+                rowEnd--;
+            }
+            
+            // Заполнение левого столбца
+            if (colStart <= colEnd)
+            {
+                for (int row = rowEnd; row >= rowStart; row--)
+                {
+                    array[row, colStart] = value;
+                    value++;
+                }
+                colStart++;
+            }
         }
-        return matrix;
-
-    double[,] CreateRandomMatrix(int m, int n, int minLimitRandom, int maxLimitRandom)
-    {
-        // Введите свое решение ниже
-        double[,] matrix = new double[m, n];
-        Random r = new Random();
-        for (int i = 0; i < m; i++)
-        {   
+        
+        // Вывод массива на консоль
+        for (int i = 0; i < n; i++)
+        {
             for (int j = 0; j < n; j++)
             {
-            matrix[i, j] = r.NextDouble() * (maxLimitRandom - minLimitRandom)+minLimitRandom;
-
+                Console.Write(array[i, j] + "\t");
             }
+            Console.WriteLine();
         }
-        return matrix;
-    }
-
-void PrintArray(double[,] matrix)
-{
-    // Введите свое решение ниже
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            System.Console.Write($"{matrix[i, j]:f2} ");
-        }
-        System.Console.WriteLine();
-    }
-}
-
-
-double[,] result = CreateRandomMatrix(6, 7, -100, 300);
-PrintArray(result);
