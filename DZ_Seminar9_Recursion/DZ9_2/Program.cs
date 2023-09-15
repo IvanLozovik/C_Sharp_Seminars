59ec943c494ceaaf45e50aa14d7ece97cd34bc08
IvanLozovik <gliderpilot19@gmail.com>
2023-09-15 23:47:58 +0300

    DZ po 9 Seminaru Recursii

---
+++ DZ_Seminar9_Recursion/DZ9_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SummMtoN (int m, int n)
+{ 
+    int summ=m;
+    if (m < n) summ+= SummMtoN(m+1, n);
+    return summ;
+}
+System.Console.WriteLine(SummMtoN (4,8));
